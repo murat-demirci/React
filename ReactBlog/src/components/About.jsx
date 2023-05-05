a03b52bfe2159fcfa8bba106376133d854166bd3
murat-demrici <ddemircimuratt@gmail.com>
2023-05-05 17:36:35 +0300

    ReactRoute

--- ReactBlog/src/components/About.jsx
+++ ReactBlog/src/components/About.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import about from "../assets/aboutus.jpg";
+import "../styles/About.css";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="main-about">
+      <div className="card-about">
+        <div
+          className="about-top"
+          style={{ backgroundImage: `url(${about})` }}
+        ></div>
+        <div className="about-content">
+          <h1>Who Are We ?</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum quam
+            saepe assumenda molestiae nam ad nemo eveniet voluptates provident
+            excepturi, iusto id veniam alias placeat, culpa ut commodi
+            veritatis! Voluptate optio, architecto ad tenetur debitis qui quae
+            facilis sunt est expedita, laudantium et unde illo quam hic maiores
+            velit quos perspiciatis excepturi amet? Maxime ab ducimus mollitia
+            quia dolore molestias, vero dicta cum. Nisi mollitia molestias
+            possimus ab corporis? Quidem repellendus esse eveniet expedita, eos
+            accusamus incidunt doloremque suscipit quaerat amet nobis harum,
+            blanditiis ex in aliquam ea vel ullam sequi perferendis explicabo!
+            Nobis, consequuntur dignissimos soluta minus officiis voluptate!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum quam
+            saepe assumenda molestiae nam ad nemo eveniet voluptates provident
+            excepturi, iusto id veniam alias placeat, culpa ut commodi
+            veritatis! Voluptate optio, architecto ad tenetur debitis qui quae
+            facilis sunt est expedita, laudantium et unde illo quam hic maiores
+            velit quos perspiciatis excepturi amet? Maxime ab ducimus mollitia
+            quia dolore molestias, vero dicta cum. Nisi mollitia molestias
+            possimus ab corporis? Quidem repellendus esse eveniet expedita, eos
+            accusamus incidunt doloremque suscipit quaerat amet nobis harum,
+            blanditiis ex in aliquam ea vel ullam sequi perferendis explicabo!
+            Nobis, consequuntur dignissimos soluta minus officiis voluptate!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- ReactBlog/src/components/Contact.jsx
+++ ReactBlog/src/components/Contact.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import '../styles/Contact.css'
+import contact from '../assets/contact.jpg'
+import SendIcon from '@mui/icons-material/Send';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='main-contact'>
+      <div className='left-side'>
+          <h1>Contact Us !</h1>
+          <form >
+            <label>Your Name</label>
+            <input type="text" name='name' autoComplete='off' placeholder='Please enter your name...'/>
+            <label>Your Email</label>
+            <input type="email" name='email' autoComplete='off' placeholder='Please enter your email...'/>
+            <label>Your Message</label>
+            <textarea rows={10} type="text" name='message'  placeholder='Please enter your message...'/>
+            <button><SendIcon/> Send </button>
+          </form>
+      </div>
+      <div className='right-side' style={{backgroundImage:`url(${contact})`}}>
+
+      </div>
+    </div>
   )
 }
