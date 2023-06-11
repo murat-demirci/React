27ac82ec475bc4ffc1df298d407e19485b5d2149
murat-demrici <ddemircimuratt@gmail.com>
2023-06-11 15:21:17 +0300

    NextJS

---
+++ NextJS/tailwind/app/page.js
@@ -0,0 +1,14 @@
+'use client'
+import Image from 'next/image'
+import { Alert } from 'flowbite-react'
+
+
+export default function HomePage() {
+  return (
+    <div>
+      Home Page
+
+      <Alert color='red'>Alert !!!!!!</Alert>
+    </div>
+  )
+}
